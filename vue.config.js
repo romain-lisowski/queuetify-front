06a1150a29d7ecc7688cc89e473e5896af99dfc6
Bastien Lemeunier <bastienlemeunier@bastien-2.local>
2020-03-31 17:15:49 +0200

    init assets

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,28 @@ module.exports = {
     resolve: {
       alias: require("./aliases.config").webpack
     }
+  },
+  css: {
+    loaderOptions: {
+        sass: {
+            data: `
+                @import "@/assets/scss/00_var/_color.scss";
+                @import "@/assets/scss/00_var/_font.scss";
+                @import "@/assets/scss/00_var/_media-queries.scss";
+                @import "@/assets/scss/01_base/_misc.scss";
+                @import "@/assets/scss/01_base/_reset.scss";
+                @import "@/assets/scss/01_base/_root.scss";
+            `
+        }
+    }
+  },
+  chainWebpack: (config) => {
+    const svgRule = config.module.rule('svg');
+
+    svgRule.uses.clear();
+
+    svgRule
+    .use('vue-svg-loader')
+    .loader('vue-svg-loader');
   }
 };
