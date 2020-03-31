a8f3b9c14bffc16ab8e7af93a74c5cbbf50b99bc
Romain Lisowski <romain.lisowski@gmail.com>
2020-03-31 17:13:19 +0200

    Add playlist tracks informations

--- src/views/Room.vue
+++ src/views/Room.vue
@@ -2,30 +2,36 @@
   <div class="container">
     <h1>Room 1 :</h1>
     <ul>
-      <li v-for="(track, index) of room.tracks" :key="index">
-        {{ track }}
+      <li v-for="(track, index) of tracks" :key="index">
+        <strong>Artist:</strong> {{ track.artists[0].name }}, <strong>Track :</strong> {{ track.name }}, <strong>Duration (ms) :</strong> {{ track.duration_ms }}
       </li>
     </ul>
   </div>
 </template>
 
 <script>
-import { db } from "@/firebase";
-
 export default {
   name: "Room",
-  data() {
-    return {
-      room: []
-    };
+  computed: {
+    tracks() {
+      return this.$store.state.tracks;
+    }
   },
-  firestore() {
-    return {
-      room: db.collection("rooms").doc("room1")
-    };
+  serverPrefetch: {
+    tracks() {
+      return this.fetchTracks();
+    }
   },
   mounted() {
+    if (!this.tracks) {
+      this.fetchTracks();
+    }
     this.$store.dispatch("initPlayer");
+  },
+  methods: {
+    fetchTracks() {
+      return this.$store.dispatch("getTracks");
+    }
   }
 };
 </script>
