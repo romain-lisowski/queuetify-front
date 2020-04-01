3e7d9356ec50532979ae8fd4a1146bfe0f9ec4d0
Romain Lisowski <romain.lisowski@gmail.com>
2020-04-01 16:08:21 +0200

    Lint

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,28 @@
 <template>
   <div class="home">
-
     <div class="home__logo">
-      <Logo/>
+      <Logo />
     </div>
 
     <div class="home__content">
-      <h1 class="headline">The collaborative listening<br>room using Spotify<span>®</span> </h1>
-      <button class="btn btn-main" @click="connexion">Connect with Spotify</button>
-      
+      <h1 class="headline">
+        The collaborative listening<br />room using Spotify<span>®</span>
+      </h1>
+      <button class="btn btn-main" @click="connexion">
+        Connect with Spotify
+      </button>
     </div>
 
     <footer class="home__footer">
-        <div class="info">We do not store any personal data.</div>
-        <div class="copyrights">
-          Code by <a href="#">Romain Lisowski</a>, Design by <a href="#">Bastien Lemeunier</a>. Get it on <a href="https://github.com/romain-lisowski">Github</a>
-        </div>
+      <div class="info">We do not store any personal data.</div>
+      <div class="copyrights">
+        Code by <a href="#">Romain Lisowski</a>, Design by
+        <a href="#">Bastien Lemeunier</a>. Get it on
+        <a href="https://github.com/romain-lisowski">Github</a>
+      </div>
     </footer>
   </div>
 </template>
-
 
 <script>
 import Logo from "@/assets/svg/logo.svg";
@@ -28,8 +31,8 @@ import SpotifyAuthLib from "@/lib/SpotifyAuth";
 
 export default {
   name: "Home",
-   components: {
-      Logo
+  components: {
+    Logo
   },
   methods: {
     connexion() {
@@ -40,5 +43,5 @@ export default {
 </script>
 
 <style lang="scss">
-    @import "@/assets/scss/03_page/_home.scss";
+@import "@/assets/scss/03_page/_home.scss";
 </style>
