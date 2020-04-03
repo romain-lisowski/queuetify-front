aa9e2771e7cab4dd4dfc0948dd1e05f3de56e1b6
Bastien Lemeunier <bastienlemeunier@bastien-2.home>
2020-04-03 16:51:11 +0200

    ça marche pas narvalo

--- src/App.vue
+++ src/App.vue
@@ -17,62 +17,6 @@ export default {
       return this.$el.querySelector(".cursor");
     }
   },
-  mounted() {
-    (function() {
-      const hoverThis = document.querySelectorAll(".hover-this");
-      const hoverThisSmall = document.querySelectorAll(".hover-this-small");
-      const hoverVote = document.querySelectorAll(".hover-vote");
-      const cursor = document.querySelector(".cursor");
-
-      const animateHoverVote = function(e) {
-        cursor.style.transform = "scale(4)";
-        cursor.classList.add("hover");
-        console.log("aaaa");
-        if (e.type === "mouseleave") {
-          cursor.style.transform = "scale(1)";
-          cursor.classList.remove("hover");
-        }
-      };
-
-      const animateHoverThis = function(e) {
-        const span = this.querySelector("span");
-        const { offsetX: x, offsetY: y } = e,
-          { offsetWidth: width, offsetHeight: height } = this,
-          move = 15,
-          xMove = (x / width) * (move * 2) - move,
-          yMove = (y / height) * (move * 2) - move;
-
-        span.style.transform = `translate(${xMove}px, ${yMove}px)`;
-        cursor.style.transform = `scale(4)`;
-        cursor.classList.add("hover");
-        if (e.type === "mouseleave") {
-          span.style.transform = "";
-          cursor.style.transform = `scale(1)`;
-          cursor.classList.remove("hover");
-        }
-      };
-
-      const animateHoverThisSmall = function(e) {
-        cursor.style.transform = "scale(3)";
-        if (e.type === "mouseleave") cursor.style.transform = "scale(1)";
-      };
-
-      hoverThis.forEach(b => b.addEventListener("mousemove", animateHoverThis));
-      hoverThis.forEach(b =>
-        b.addEventListener("mouseleave", animateHoverThis)
-      );
-      hoverThisSmall.forEach(b =>
-        b.addEventListener("mousemove", animateHoverThisSmall)
-      );
-      hoverThisSmall.forEach(b =>
-        b.addEventListener("mouseleave", animateHoverThisSmall)
-      );
-      hoverVote.forEach(b => b.addEventListener("mousemove", animateHoverVote));
-      hoverVote.forEach(b =>
-        b.addEventListener("mouseleave", animateHoverVote)
-      );
-    })();
-  },
   methods: {
     mouseMove(e) {
       const { clientX: x, clientY: y } = e;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,7 +8,12 @@
       <h1 class="headline">
         The collaborative listening<br />room using Spotify<span>®</span>
       </h1>
-      <button class="btn btn-main hover-this" @click="connexion">
+      <button
+        class="btn btn-main hover-this"
+        @click="connexion"
+        @mousemove="animateHoverThis"
+        @mouseleave="animateHoverThis"
+      >
         <span>
           Connect with Spotify
           <ButtonArrow />
@@ -20,11 +25,13 @@
       <div class="copyrights">
         Code by
         <a class="btn btn-inline hover-this-small" href="#"
-          >Romain Lisowski<ButtonArrow /></a
-        >, Design by
+          >Romain Lisowski<ButtonArrow />
+        </a>
+        , Design by
         <a class="btn btn-inline hover-this-small" href="#"
-          >Bastien Lemeunier<ButtonArrow /></a
-        >. Get it on
+          >Bastien Lemeunier<ButtonArrow />
+        </a>
+        . Get it on
         <a
           class="btn btn-inline hover-this-small"
           href="https://github.com/romain-lisowski"
@@ -47,9 +54,31 @@ export default {
     Logo,
     ButtonArrow
   },
+  computed: {
+    cursor() {
+      return this.$el.querySelector(".cursor");
+    }
+  },
   methods: {
     connexion() {
       SpotifyAuthLib.getAuthorization();
+    },
+    animateHoverThis(e) {
+      const span = this.querySelector("span");
+      const { offsetX: x, offsetY: y } = e,
+        { offsetWidth: width, offsetHeight: height } = this,
+        move = 15,
+        xMove = (x / width) * (move * 2) - move,
+        yMove = (y / height) * (move * 2) - move;
+
+      span.style.transform = `translate(${xMove}px, ${yMove}px)`;
+      this.cursor.style.transform = `scale(4)`;
+      this.cursor.classList.add("hover");
+      if (e.type === "mouseleave") {
+        span.style.transform = "";
+        this.cursor.style.transform = `scale(1)`;
+        this.cursor.classList.remove("hover");
+      }
     }
   }
 };
