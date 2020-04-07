e622316763c5f469c937b3fb9ea4d40421da76aa
Romain Lisowski <romain.lisowski@gmail.com>
2020-04-07 04:46:07 +0200

    Clean server

--- server/app.js
+++ server/app.js
@@ -4,9 +4,17 @@ var io = require("socket.io")(http);
 
 // eslint-disable-next-line no-unused-vars
 io.on("connection", socket => {
-  console.log("Connected : ", socket.id);
+  console.log("CONNECTED : ", socket.id);
+  io.emit("CONNECTED", socket.id);
+
   socket.on("disconnect", () => {
-    console.log("Disconnected : ", socket.id);
+    console.log("DISCONNECTED : ", socket.id);
+    io.emit("DISCONNECTED", socket.id);
+  });
+
+  socket.on("E_USER_CONNECTED", data => {
+    console.log("E_USER_CONNECTED : " + socket.id + "-" + data);
+    io.emit("USER_CONNECTED", data);
   });
 
   socket.on("E_VOTE_TRACK", () => {
@@ -20,7 +28,7 @@ io.on("connection", socket => {
   });
 
   socket.on("E_NEXT_TRACK", data => {
-    console.log("E_NEXT_TRACK : " + socket.id, data);
+    console.log("E_NEXT_TRACK : " + socket.id + "-" + data);
     io.emit("NEXT_TRACK", data);
   });
 });
